622d8d134aa1e478e00121c93cc4d010df9459f3
Dregoth <xanton.ac@gmail.com>
2019-09-02 18:07:19 +0200

    "Working buttons, lacks showing results"

--- script.js
+++ script.js
@@ -1,100 +1,148 @@
-        var p1_score = 0;
-        var cpu_score = 0;
-        var tie_score = 0;
+var p1_score;
+var cpu_score;
+var tie_score;
+var x;
+var y;
+var rock;
+var paper;
+var scissors;
 
-        function computerPlay() {
-            var computerSelection = Math.ceil(Math.random() * 3);
+window.onload = function () {
+    x = document.getElementById("button-area");
+    x.style.display = "none";
+    y = document.getElementById("play-area");
+    rock = document.getElementById('rock');
+    paper = document.getElementById('paper');
+    scissors = document.getElementById('scissors');
 
-            switch (computerSelection) {
 
-                case 1: computerSelection = 'rock';
-                    break;
-                case 2: computerSelection = 'paper';
-                    break;
-                case 3: computerSelection = 'scissors';
-                    break;
+    /* adds events to every button and calls playRound() on click... */
+    rock.addEventListener('click', function () {
+        playRound('rock', computerPlay());
 
+    });
+
+    paper.addEventListener('click', function () {
+        playRound('paper', computerPlay());
+    });
+
+    scissors.addEventListener('click', function () {
+        playRound('scissors', computerPlay());
+    });
+
+
+}
+
+function TooglePlay() {
+
+    p1_score = 0;
+    cpu_score = 0;
+    tie_score = 0;
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        document.getElementById("play").innerHTML = "Play again!";
+        document.getElementById("p1score").innerHTML = p1_score;
+        document.getElementById("cpuscore").innerHTML = cpu_score;
+        document.getElementById("tiescore").innerHTML = tie_score;
+        y.style.display = "none";
+
+
+    } else {
+        x.style.display = "none";
+        y.style.display = "block";
+    }
+
+}
+
+
+
+
+function computerPlay() {
+    var computerSelection = Math.ceil(Math.random() * 3);
+
+    switch (computerSelection) {
+
+        case 1: computerSelection = 'rock';
+            break;
+        case 2: computerSelection = 'paper';
+            break;
+        case 3: computerSelection = 'scissors';
+            break;
+
+    }
+    console.log('CPU: ' + computerSelection);
+    return computerSelection;
+}
+
+function playRound(playerSelection, computerSelection) {
+    var win_quote = "You win! " + playerSelection.toUpperCase() + " beats " + computerSelection.toUpperCase() + "!";
+    var lose_quote = "You lose! " + playerSelection.toUpperCase() + " loses to " + computerSelection.toUpperCase() + "!";
+
+
+    if (playerSelection == computerSelection) {
+        tie_score++;
+        checkResult(p1_score, cpu_score);
+        console.log("It's a DRAW! " + playerSelection.toUpperCase() + " vs. " + computerSelection.toUpperCase() + "!");
+    } else switch (playerSelection) {
+        case 'rock':
+            if (computerSelection == 'paper') {
+                cpu_score++;
+                console.log(lose_quote);
+                console.log("CPU: " + cpu_score);
+            } else {
+                p1_score++;
+                console.log(win_quote);
+                console.log("P1: " + p1_score);
             }
-            console.log('CPU: ' + computerSelection);
-            return computerSelection;
-        }
+            checkResult(p1_score, cpu_score);
+            break;
 
-
-        function playerPlay() {
-
-            var playerSelection = prompt('Choose your play!');
-            playerSelection = playerSelection.toLowerCase();
-            console.log('Player: ' + playerSelection);
-            return playerSelection;
-        }
-
-
-        function playRound(playerSelection, computerSelection) {
-            var win_quote = "You win! " + playerSelection.toUpperCase() + " beats " + computerSelection.toUpperCase() + "!";
-            var lose_quote = "You lose! " + playerSelection.toUpperCase() + " loses to " + computerSelection.toUpperCase() + "!";
-
-            if (playerSelection == computerSelection) {
-                tie_score++;
-                console.log ("It's a DRAW! " + playerSelection.toUpperCase() + " vs. " + computerSelection.toUpperCase() + "!");
-            } else switch (playerSelection) {
-                case 'rock':
-                    if (computerSelection == 'paper') {
-                        cpu_score++;
-                        console.log (lose_quote);
-                    } else {
-                        p1_score++;
-                        console.log (win_quote);
-                        }
-                    break;
-
-                case 'paper':
-                    if (computerSelection == 'scissors') {
-                        cpu_score++;
-                        console.log (lose_quote);
-                    } else {
-                        p1_score++;
-                        console.log (win_quote);
-                        }
-                    break;
-
-                case 'scissors':
-                    if (computerSelection == 'rock') {
-                        cpu_score++;
-                        console.log (lose_quote);
-                    } else {
-                        p1_score++;
-                        console.log (win_quote);
-                    }
-                    break;
-                default:
-                    console.log("You messed up. That's not how you play RPS!\n Try again...");
+        case 'paper':
+            if (computerSelection == 'scissors') {
+                cpu_score++;
+                console.log(lose_quote);
+                console.log("CPU: " + cpu_score);
+            } else {
+                p1_score++;
+                console.log(win_quote);
+                console.log("P1: " + p1_score);
             }
+            checkResult(p1_score, cpu_score);
+            break;
 
-
-        }
-
-
-        function game() {
-
-            for (var i = 0; i < 5; i++) {
-                var playerSelection = playerPlay();
-                var computerSelection = computerPlay();
-                playRound(playerSelection, computerSelection);
-               
+        case 'scissors':
+            if (computerSelection == 'rock') {
+                cpu_score++;
+                console.log(lose_quote);
+                console.log("CPU: " + cpu_score);
+            } else {
+                p1_score++;
+                console.log(win_quote);
+                console.log("P1: " + p1_score);
             }
-
-            if (p1_score == cpu_score) {
-                console.log("Player: " +p1_score+ ". CPU: " + cpu_score+ ". Ties: "+ tie_score+ ". Draw game!");
-
-            } else if (p1_score > cpu_score) {
-                console.log("Player: " +p1_score+ ". CPU: " + cpu_score+ ". Ties: "+ tie_score+ ". You win!");
-            }else {
-                console.log("Player: " +p1_score+ ". CPU: " + cpu_score+ ". Ties: "+ tie_score+ ". You lose!");
-            }
- 
-
-        }
-
-        game();
+            checkResult(p1_score, cpu_score);
+            break;
+        default:
+            console.log("You messed up. That's not how you play RPS!\n Try again...");
+    }
 
 
+}
+
+function checkResult(p1_score, cpu_score) {
+
+    document.getElementById("p1score").innerHTML = p1_score;
+    document.getElementById("cpuscore").innerHTML = cpu_score;
+    document.getElementById("tiescore").innerHTML = tie_score;
+    if (p1_score == 5) {
+        console.log("************** VICTORY! P1: " + p1_score + " CPU: " + cpu_score);
+        TooglePlay();
+    } else if (cpu_score == 5) {
+        console.log("************** DEFEAT! P1: " + p1_score + " CPU: " + cpu_score);
+        TooglePlay();
+    }
+
+
+
+}
